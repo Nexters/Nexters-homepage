cde6acefc9eabbc99b57b157be66e524ede302e1
daehyun-p <dhp0102@gmail.com>
2014-12-27 15:32:57 +0900

    Add a StaticPage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'statig_pages/home'
+
+  get 'statig_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/helpers/statig_pages_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StatigPagesHelperTest < ActionView::TestCase
+end
